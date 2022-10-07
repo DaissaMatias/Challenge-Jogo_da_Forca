0038703a7d47cdd83d8b336a647c6ba62b1eb4c2
Daissa Matias <daissamatias@gmail.com>
2022-10-07 09:26:44 -0300

    Mudança do nome da variável tablero para tabuleiro

--- canvas.js
+++ canvas.js
@@ -1,28 +1,28 @@
 function desenharCanvas() {
-    tablero.lineWidth=8
-    tablero.lineCap="round"
-    tablero.lineJoin="round"
-    tablero.fillStyle="#242424"
-    tablero.strokeStyle="func"
-    tablero.fillRect(0,0,1200,800);
-    tablero.beginPath();
-    tablero.moveTo(650,500);
-    tablero.lineTo(900,500);
-    tablero.stroke();
-    tablero.closePath();
+    tabuleiro.lineWidth=8
+    tabuleiro.lineCap="round"
+    tabuleiro.lineJoin="round"
+    tabuleiro.fillStyle="#242424"
+    tabuleiro.strokeStyle="func"
+    tabuleiro.fillRect(0,0,1200,800);
+    tabuleiro.beginPath();
+    tabuleiro.moveTo(650,500);
+    tabuleiro.lineTo(900,500);
+    tabuleiro.stroke();
+    tabuleiro.closePath();
 }
 
 function desenharLinhas() {
-    tablero.lineWidth=6
-    tablero.lineCap="round"
-    tablero.lineJoin="round"
-    tablero.strokeStyle="#E1B25C"
-    tablero.beginPath();
+    tabuleiro.lineWidth=6
+    tabuleiro.lineCap="round"
+    tabuleiro.lineJoin="round"
+    tabuleiro.strokeStyle="#E1B25C"
+    tabuleiro.beginPath();
     let largura=600/palavraSecreta.length
     for (let i=0;i<palavraSecreta.length;i++){
-        tablero.moveTo(500+(largura*i),640)
-        tablero.lineTo(550+(largura*i),640)
+        tabuleiro.moveTo(500+(largura*i),640)
+        tabuleiro.lineTo(550+(largura*i),640)
     }
-    tablero.stroke();
-    tablero.closePath();
+    tabuleiro.stroke();
+    tabuleiro.closePath();
 }
