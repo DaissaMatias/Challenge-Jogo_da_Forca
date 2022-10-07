5b499bc1e0ff08974ef48aeb8354a259d7bef2bf
Daissa Matias <daissamatias@gmail.com>
2022-10-07 09:27:40 -0300

    Declaração dos seletores, (let e var).

---
+++ forca.js
@@ -0,0 +1,17 @@
+let tela = document.querySelector("canvas");
+let btnNovoJogoDesaparecido = document.getElementById("btn-novo-jogo").style.display = "none";
+let btnSairDesaparecer = document.getElementById("btn-sair").style.display = "none";
+let divAgregarPalavra = document.getElementById("adicionar-palavra").style.display = "none";
+let btnNovoJogo = document.getElementById("btn-novo-jogo");
+let btnSair = document.getElementById("btn-sair");
+let btnCancelar = document.getElementById("btn-cancelar");
+
+var palavras = ['ALURA','FORCA','HTML','ORACLE','JAVASCRIPT','LOGICA','PROGRAMA','DESAFIO'];
+var tabuleiro = document.getElementById('forca').getContext('2d');
+var palavraSecreta = "";
+var letras = [];
+var palavraCorreta = "";
+var erros = 8;
+let letrasIncorretas = [];
+let numeroDeErros = 8;
+let LetraEscolhida = [];
